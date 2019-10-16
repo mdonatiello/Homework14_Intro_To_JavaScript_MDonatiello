ce5e9c5fcc91f41cf3622f1e4cf06fd88313abba
Marc Donatiello <marc.donatiello@gmail.com>
2019-10-16 11:42:19 -0400

    Committing files for grading.

--- static/js/app.js
+++ static/js/app.js
@@ -1,43 +1,52 @@
-// Assign the data from `data.js` to a descriptive variable
-var $tbody = document.querySelector("tbody");
-var $datetimeInput = document.querySelector("#datetime");
-var $button = document.querySelector("#filter-btn");
+// from data.js
+var tableData = data;
+
+// Display all UFO sightings
+function tableDisplay(sightings) {
+  var tbody = d3.select("tbody");
+  sightings.forEach((dateData) => {
+    var row = tbody.append("tr");
+  Object.entries(dateData).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+};
+
+function deleteTbody() {
+  d3.select("tbody")
+    .selectAll("tr").remove()
+    .selectAll("td").remove();
+};
+
+tableDisplay(tableData);
 
 // Select the button
-$button.addEventListener("click", handleSearchButtonClick);
+var button = d3.select("#filter-btn");
 
-var filteredData = data;
+// Filter and display the results
+button.on("click", function(event) {
+    d3.event.preventDefault();
+    deleteTbody();
+    // Select the input element and get the raw HTML code
+    var inputValue = d3.select("#datetime").property("value");
 
-function renderTable() {
-  $tbody.innerHTML = "";
-  for (var i = 0; i < filteredData.length; i++) {
-    var data = filteredData[i];
-    var fields = Object.keys(data);
-    var $row = $tbody.insertRow(i);
-    for (var j = 0; j < fields.length; j++) {
-      var field = fields[j];
-      var $cell = $row.insertCell(j);
-      $cell.innerText = data[field];
-    }
-  }
-}
+    // filter data on user input and display filtered data or display all data if date box has no date in it
+    if (inputValue.trim() === "") {
+      var filteredData = tableData;
+    } else {
+      var filteredData = tableData.filter(sightings => sightings.datetime === inputValue.trim());
+  };
 
-function handleSearchButtonClick() {
-  // Format the user's search by removing leading and trailing whitespace, lowercase the string
-  var filterDatetime = $datetimeInput.value.trim().toLowerCase();
+// If no records found
+  if (filteredData.length == 0) {
+    d3.select("tbody")
+      .append("tr")
+      .append("td")
+        .attr("colspan", 7)
+        .html("<h4>No Records Found</h4>");
+  };
 
-// Set filteredData to an array of all addresses who's "state" matches the filter
-filteredData = data.filter(function(data) {
-  var dataDatetime = data.datetime.substring(0, filterDatetime.length).toLowerCase();
-  if (dataDatetime === filterDatetime) {
-    return true;
-  }
-  return false;
-  });
- 
-renderTable();
-}
-
-renderTable();
-
+  tableDisplay(filteredData);
+});
 
